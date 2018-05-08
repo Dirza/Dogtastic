c78b3729b508f05a06db02af6f2f5c744a88878f
Natália Vápár <a01304331@unet.univie.ac.at>
2018-05-08 14:09:36 +0200

    first commit

--- app/src/main/java/hci/univie/ac/at/dogtastic/Home.java
+++ app/src/main/java/hci/univie/ac/at/dogtastic/Home.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class Home extends AppCompatActivity {
 
-    //comment
+    //comment AGAIN
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
