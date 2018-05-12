2a51c8f45ab5a2ddf618ce4776f634d073fa8ee4
Sandra Hofmarcher <a1404086@unet.univie.ac.at>
2018-05-12 18:55:41 +0200

    Activity für Statistik angelegt

---
+++ app/src/main/java/hci/univie/ac/at/dogtastic/activity_Stats.java
@@ -0,0 +1,13 @@
+package hci.univie.ac.at.dogtastic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_Stats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__stats);
+    }
+}
